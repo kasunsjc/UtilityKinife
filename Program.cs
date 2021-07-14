38ef52373862d94bae72df57b5f5cb51ce02a371
Kasun Rajapakse <kasunsjc@hotmail.com>
2021-07-14 16:20:53 +0530

    Inetial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UtilityKinife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
