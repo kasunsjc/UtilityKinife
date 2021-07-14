7063a90ef3e54bdfc27f5d26e4007fc1590ad3b9
Kasun Rajapakse <kasunsjc@hotmail.com>
2021-07-14 16:35:48 +0530

    Added new class for Temp Convertor TempConvertor.cs
    On branch temperatureConverter

---
+++ Convertors/TempConvertor.cs
@@ -0,0 +1,12 @@
+namespace UtilityKinife.Convertors
+{
+    public class FahrenhietToCelsius
+    {
+        public double FahrenhietToCelsiusConvert(double fahrenheitTemp)
+        {
+            double _fahrenhietTemp = fahrenheitTemp= 0.0;
+            double _celsius = 0.0;
+            return _celsius;
+        }
+    }
+}
